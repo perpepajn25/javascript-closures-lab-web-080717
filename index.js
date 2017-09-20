369dd114296850ed45b2f6c376b1e6f98899e64a
Natalie Perpepaj <perpepajn25@gmail.com>
2017-09-20 15:18:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  let counter = 0;
+  return {
+    addBump: () =>{
+      counter+=1
+    },
+    getBumps: () => {
+      return counter
+    }
+  }
+}
+
+function createAnimal(animalType){
+  return (deadlyDevice) => {
+    return {
+      animalType,
+      deadlyDevice
+    }
+  }
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
